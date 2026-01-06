6fbbbb1e211d90246e18fd9b186cd6518f064c61
lyfe2025 <9579249@qq.com>
2026-01-06 09:05:58 +0800

    fix(hover-card): 修复属性继承问题并合并$attrs
    添加inheritAttrs: false选项防止属性自动继承
    合并$attrs到forwardedProps确保所有属性正确传递

--- web/src/components/ui/hover-card/HoverCardContent.vue
+++ web/src/components/ui/hover-card/HoverCardContent.vue
@@ -5,6 +5,10 @@ import { reactiveOmit } from '@vueuse/core'
 import { HoverCardContent, HoverCardPortal, useForwardProps } from 'reka-ui'
 import { cn } from '@/lib/utils'
 
+defineOptions({
+  inheritAttrs: false,
+})
+
 const props = withDefaults(
   defineProps<HoverCardContentProps & { class?: HTMLAttributes['class'] }>(),
   {
@@ -20,7 +24,7 @@ const forwardedProps = useForwardProps(delegatedProps)
 <template>
   <HoverCardPortal>
     <HoverCardContent
-      v-bind="forwardedProps"
+      v-bind="{ ...$attrs, ...forwardedProps }"
       :class="
         cn(
           'z-50 w-64 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
